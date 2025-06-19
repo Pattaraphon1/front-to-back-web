a83762a153dcfd3497d470422924ff71e41ef4a7
pattaraphon1 <klgsa51@gmail.com>
2025-06-19 15:40:42 +0700

    register validate and button alert complete

--- src/components/form/FormInput.jsx
+++ src/components/form/FormInput.jsx
@@ -1,9 +1,17 @@
-function FormInput({name, register}) {
+function FormInput({name, register, errors, type="text"}) {
   return (
+    <>
       <input className='border w-full rounded-md border-gray-400 p-1 px-4' 
       placeholder={name}
+      type={type}
       {...register(name)}
       />
+      <p>
+        {
+        errors[name] && <p className="text-red-500 text-sm">{errors[name].message}</p>
+        }
+      </p>
+      </>
   )
 }
 export default FormInput
---
+++ src/utils/validator.jsx
@@ -0,0 +1,15 @@
+// Validate with yup
+import {object, ref, string} from "yup";
+
+export const registerSchema = object({
+  email: string().email("Email ไม่ถูกต้อง").required("กรุณากรอกอีเมลล์"),
+  name: string().min(2,"Name ต้องมากกว่า 2 อักขระ"),
+  password: string().min(6, "Password ต้องมากกว่า 6"),
+  confirmPassword: string().oneOf(
+    [ref("password"),null],"Confirm Password ไม่ตรงกัน"),
+});
+
+export const loginSchema = object({
+  email: string().email("Email ไม่ถูกต้อง").required("กรุณากรอกอีเมลล์"),
+  password: string().min(6, "Password ต้องมากกว่า 6"),
+});
